c5ed4ec9ff4e26e6efbf874df22ca40305931608
ian firmino <ian.firmino@hotmail.com>
2023-03-15 07:37:53 -0300

    ajuste porta

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const router = require('./routes/router');
+const port = 8686;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use("/", router);
 
-app.listen(8080, () => {
-    console.log('Servidor rodando!')
+app.listen(port, () => {
+    console.log('Servidor rodando! na porta ' + port)
 });
 
  
