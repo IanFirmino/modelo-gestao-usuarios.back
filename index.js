55874321f9ea73584592c4774d5e671714249994
ian firmino <ian.firmino@hotmail.com>
2023-03-15 07:41:16 -0300

    cors

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = require('./routes/router');
 const port = 8686;
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use(cors());
 app.use("/", router);
 
 app.listen(port, () => {
